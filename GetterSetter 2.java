d8b1951fec11446969bfca5926e425170f891993
221B312 <221B312@juet.co.in>
2025-01-22 14:19:57 +0530

    second getter setter code

---
+++ GetterSetter 2.java
@@ -0,0 +1,38 @@
+public class LE0 {
+    private String name;
+    private int speed;
+
+    public CAR(String name, int speed) {
+        this.name = name;
+        this.speed = speed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public static void main(String[] args) {
+        CAR car1 = new CAR("Gloster", 80);
+
+        System.out.println("Car Name: " + car1.getName()); 
+        System.out.println("Car Speed: " + car1.getSpeed() + " km/h");  
+
+        car1.setName("Hector");
+        car1.setSpeed(100);
+
+        System.out.println("Updated Car Name: " + car1.getName()); 
+        System.out.println("Updated Car Speed: " + car1.getSpeed() + " km/h");  
+    }
+}

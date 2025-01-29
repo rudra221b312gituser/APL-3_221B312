1d85a303682f12ee6485ecb286024983439f7fdd
221B312 <221b312@juetguna.in~>
2025-01-29 15:14:17 +0530

    lab 2 solutions

---
+++ two/prob1/Main.java
@@ -0,0 +1,11 @@
+class Main
+{
+
+public static void main(String [] args)
+{
+  Voice voice = new Voice();
+  voice.prepareVoice();
+  //voice.hear();
+
+}
+}
---
+++ two/prob1/Voice.java
@@ -0,0 +1,25 @@
+class Voice
+{
+   
+void prepareVoice() 
+{
+        Animal[] animals = new Animal[5];
+        animals[0] = new Cow();
+        animals[1] = new Dog();
+        animals[2] = new Pig();
+        animals[3] = new Goat();
+        animals[4] = new Lion();
+        hear(animals);
+}
+
+    void hear(Animal[] animals) 
+{
+        for (int i=0;i<5;i++) 
+{
+            animals[i].makeVoice();
+}
+}
+}
+
+
+
